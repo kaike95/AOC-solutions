c97ca4a2445d3412a45154c4536afa433dd15719
kaike95 <kaike@tuta.io>
2023-01-18 16:04:10 -0300

    add part1-2 for 2022/Day4

---
+++ 2022/Day4/part1-2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+[[ -z $1 ]] && { echo "you need to specify the challenge input: ${0} input.txt"; exit; }
+[[ ! -s $1 ]] && { echo "challenge input file is empty or does not exist"; exit; }
+inputfile="$1"; shift
+
+# i learned how to use IFS
+
+while IFS="-," read -ra input; do
+  
+  if [[ ${input[0]} -le ${input[2]} && ${input[1]} -ge ${input[3]} ]] \
+  || [[ ${input[0]} -ge ${input[2]} && ${input[1]} -le ${input[3]} ]]; then
+
+    ((counter++))
+  
+  fi
+ 
+  if [[ ${input[0]} -le ${input[2]} && ${input[1]} -ge ${input[3]} ]] \
+  || [[ ${input[0]} -ge ${input[2]} && ${input[1]} -le ${input[3]} ]] \
+  || [[ ${input[0]} -le ${input[3]} && ${input[1]} -ge ${input[2]} ]]; then
+    ((overlap++))
+  fi
+
+done < "$inputfile"
+
+echo "$counter"
+echo "$overlap"

a35e56f307b3dcdbe14d385d68734720a61ccd5b
kaike95 <kaike@tuta.io>
2023-01-27 09:51:35 -0300

    rewrite 2022/Day1

--- 2022/Day1/part1-2.sh
+++ 2022/Day1/part1-2.sh
@@ -2,41 +2,26 @@
 
 [[ -z $1 ]] && { echo "you need to specify the challenge input: ${0} input.txt"; exit; }
 [[ ! -s $1 ]] && { echo "challenge input file is empty or does not exist"; exit; }
+inputfile=$1
 
-#backup input
-cp "$1" "copy-$1"
-
+declare -a arrtotal
 total=0
 total_three=0
 
-while [[ -s "copy-$1" ]]; do
-
-	#reads line by line
-	while read -r line; do
-		[[ -z "$line" ]] && break || { 
-			total=$(( total + line ))
-			#removes read line
-			sed -i "1d" "copy-$1"
-			
-			#this is so cool yet useless in this case, feel free to comment it
-			echo -ne "please wait, reading numbers ... $line"\\r
-		}
-	done < "copy-$1"
-
-	#remove blank space between number groups
-	sed -i "1d" "copy-$1"
-	
-	echo "$total" >> total.log
-
-	#reset total
-	total=0
-done
-
+#reads line by line
 while read -r line; do
-		total_three=$(( total_three + line ))
-	done < <(sort -nr total.log | head -n3)
+	if [[ -n "$line" ]]; then
+		total=$(( total + line ))
+	else 
+		arrtotal+=( "$total" )
+		total=0
+	fi
+done < "$inputfile"
 
-echo "Part 1: the highest number of calories is: $(sort -nr total.log | head -n1)"
-echo "Part 2: the sum of the calories of the top three elves is: $total_three" 
+sorted=( $(printf "%s\n" "${arrtotal[@]}" | sort -nr) )
 
-rm "copy-$1" "total.log"
+total_three=$(( sorted[0] + sorted[1] + sorted[2] ))
+
+echo "Part 1: the highest number of calories is: ${sorted[0]}"
+echo "Part 2: the sum of the calories of the top three elves is: $total_three"
+
